c2ec47bb8115a97b12630509338f8158105a9176
Prathmesh Kadam <kadamprathmesh530@gmail.com>
2026-08-27 12:44:32 +0530

    Fix Render proxy configuration

--- server/server.js
+++ server/server.js
@@ -16,6 +16,7 @@ const adminRoutes = require('./routes/admin');
 initDatabase();
 
 const app = express();
+app.set('trust proxy', 1); 
 const PORT = process.env.PORT || 3000;
 
 // Security Middlewares
